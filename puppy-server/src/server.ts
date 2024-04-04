e27e475d7dfe01435a36be1316548af5d2d619cc
Guilherme Marques <marquesguilherme3@gmail.com>
2024-04-04 16:35:46 -0300

    Broadcast any message

--- puppy-server/src/server.ts
+++ puppy-server/src/server.ts
@@ -1,5 +1,30 @@
 import net from 'node:net'
 
-export const server = net.createServer(socket => {
-  socket.pipe(socket)
-})
+const sockets = new Set<net.Socket>()
+
+function broadcast(sender: net.Socket, message: Buffer) {
+  sockets.forEach(socket => {
+    if (socket === sender) return
+    socket.write(message)
+  })
+}
+
+function getSocketAddress(socket: net.Socket): string {
+  const address = socket.remoteAddress
+  const port = socket.remotePort
+  const family = socket.remoteFamily
+
+  if (family === 'IPv4') return `${address}:${port}`
+  return `[${address}]:${port}`
+}
+
+function connectionHandler(socket: net.Socket) {
+  sockets.add(socket)
+
+  socket.on('data', data => {
+    console.log(`Broadcasting message from ${getSocketAddress(socket)}`)
+    broadcast(socket, data)
+  })
+}
+
+export const server = net.createServer(connectionHandler)
